aee74bd683ce240cb5a00e0d8944dd4e99226328
engsousajoao <engsousa.joao@gmail.com>
2026-08-02 01:15:00 -0300

    Create firebase.js

---
+++ sistema/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDqWEqGcJN0GLW40048tIRqrN5Xb8KyS8Q",
+  authDomain: "vektor-erp.firebaseapp.com",
+  projectId: "vektor-erp",
+  storageBucket: "vektor-erp.firebasestorage.app",
+  messagingSenderId: "789561244129",
+  appId: "1:789561244129:web:4f447018ed23f5f179faa0"
+};
